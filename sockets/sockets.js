73253dac2bca8da1130189c7a32ca902087a3e3a
Kobi Cummings <kobicummings@gmail.com>
2019-10-14 13:15:03 -0700

    changes in sockets

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -7,7 +7,6 @@
 //   client.on('disconnect', () => { /* … */ });
 // });
 
-
 //Database3306
 
 // server.listen(3000);
@@ -19,40 +18,38 @@
 // server.listen(3000)
 // setting up and connecting socket io with express
 
-
-
-
-
-
 var app = require("express")();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-app.get("/", function (req, res) {
-   res.sendfile("index.html");
+app.get("/", function(req, res) {
+  res.sendfile("index.html");
 });
 
 users = [];
-io.on("connection", function (socket) {
-   console.log("A user connected");
-   socket.on("setUsername", function (data) {
-      console.log(data);
-      if (users.indexOf(data) > -1) {
-         socket.emit("userExists", data + " username is taken! Try some other username.");
-      } else {
-         users.push(data);
-         socket.emit("userSet", {
-            username: data
-         });
-      }
-   });
+io.on("connection", function(socket) {
+  console.log("A user connected");
+  socket.on("setUsername", function(data) {
+    console.log(data);
+    if (users.indexOf(data) > -1) {
+      socket.emit(
+        "userExists",
+        data + " username is taken! Try some other username."
+      );
+    } else {
+      users.push(data);
+      socket.emit("userSet", {
+        username: data
+      });
+    }
+  });
 
-   socket.on("msg", function (data) {
-      //Send message to everyone
-      io.sockets.emit("newmsg", data);
-   })
+  socket.on("msg", function(data) {
+    //Send message to everyone
+    io.sockets.emit("newmsg", data);
+  });
 });
 
-http.listen(3000, function () {
-   console.log("listening on localhost:3000");
+http.listen(3000, function() {
+  console.log("listening on localhost:3000");
 });
