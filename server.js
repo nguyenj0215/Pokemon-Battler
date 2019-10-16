519d54b81b64174c10cf81cb7681e3dc7ad77ac2
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-15 19:45:32 -0700

    rebuilt code to incorporate socket

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 require("dotenv").config();
-var express = require("express");
 
+/* eslint-disable import/no-absolute-path */
+/* eslint-disable import/no-unresolved */
+const express = require('express');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 var db = require("./models");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+// var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+let rooms = 0;
 
 //Static directory
 app.use(express.static("public"));
@@ -24,16 +31,45 @@ var syncOptions = { force: false };
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
-
+  
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+  //Connection to socket
+  io.on('connection', (socket) => {
+    // Create a new game room and notify the creator of game.
+    socket.on('createGame', (data) => {
+      socket.join(`room-${++rooms}`);
+      socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
+    });
+  
+    // Connect the Player 2 to the room he requested. Show error if room full.
+    socket.on('joinGame', (data) => {
+      const room = io.nsps['/'].adapter.rooms[data.room];
+      if (room && room.length === 1) {
+        socket.join(data.room);
+        socket.broadcast.to(data.room).emit('player1', {});
+        socket.emit('player2', { name: data.name, room: data.room });
+      } else {
+        socket.emit('err', { message: 'Sorry, The room is full!' });
+      }
+    });
+  
+    socket.on('playTurn', (data) => {
+      socket.broadcast.to(data.room).emit('turnPlayed', {
+        tile: data.tile,
+        room: data.room,
+      });
+    });
+  
+    socket.on('gameEnded', (data) => {
+      socket.broadcast.to(data.room).emit('gameEnd', data);
+    });
   });
+
+  server.listen(process.env.PORT || 8080, function() {
+      console.log("🌎  Listening on port 8080. Visit http://localhost:8080/ in your browser."
+      );
+    });
 });
 
 module.exports = app;
