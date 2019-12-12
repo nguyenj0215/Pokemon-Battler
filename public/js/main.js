c2bba49b22b525c372c270aadd676859e970fecc
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-12-11 19:02:23 -0800

    fixed routing

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
   //For local hosting use
-  //const socket = io.connect('localhost:8080');
+  const socket = io.connect('localhost:8080');
   //For Heroku use 
-  var socket = io.connect();
+  //var socket = io.connect();
 
 var pokemonArry = [
   {
